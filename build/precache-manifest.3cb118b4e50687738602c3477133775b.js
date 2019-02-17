6f02382c8fa03119571fd8227666d80431c17517
karenmargaryan97 <karen.margaryan.2015@gmail.com>
2019-02-18 00:36:43 +0400

    prod

---
+++ build/precache-manifest.3cb118b4e50687738602c3477133775b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6ccfdb6500c44e4ddf78",
+    "url": "/static/js/main.6ccfdb65.chunk.js"
+  },
+  {
+    "revision": "2e70bef4e970b95032db",
+    "url": "/static/js/2.2e70bef4.chunk.js"
+  },
+  {
+    "revision": "6ccfdb6500c44e4ddf78",
+    "url": "/static/css/main.b52ed200.chunk.css"
+  },
+  {
+    "revision": "64c6806d3ce2657a856bdb98e2350770",
+    "url": "/index.html"
+  }
+];
